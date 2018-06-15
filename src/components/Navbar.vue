f92503c942683180b2d00e3554b615b2521658de
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2018-06-15 14:21:32 -0300

    Fixed header

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,58 +1,54 @@
 <template>
   <div>
+    <div class="header">
+      <b-navbar toggleable="md">
+        <b-nav-toggle target="nav_collapse"></b-nav-toggle>
 
-  <b-navbar toggleable="md">
+        <b-navbar-brand :to="{name: 'Home'}">DistroWatch</b-navbar-brand>
 
-    <b-nav-toggle target="nav_collapse"></b-nav-toggle>
+        <b-collapse is-nav id="nav_collapse">
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search" />
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
 
-    <b-navbar-brand :to="{name: 'Home'}">DistroWatch</b-navbar-brand>
+          <b-nav is-nav-bar>
+            <b-nav-item :to="{name: 'Distro'}">FreeBSD</b-nav-item>
+          </b-nav>
 
-    <b-collapse is-nav id="nav_collapse">
+          <!-- Right aligned nav items -->
+          <b-nav is-nav-bar class="ml-auto">
+            <b-nav-item-dropdown text="Main topics" right>
+              <b-dropdown-item :to="{name: 'Headlines'}">Headlines</b-dropdown-item>
+              <b-dropdown-item :to="{name: 'Weekly'}">DW Weekly</b-dropdown-item>
+              <b-dropdown-item disabled>Comments</b-dropdown-item>
+              <hr>
+              <b-dropdown-item :to="{name: 'Packages'}">Packages</b-dropdown-item>
+              <b-dropdown-item :to="{name: 'PackageManagement'}">Package Management</b-dropdown-item>
+              <hr>
+              <b-dropdown-item :to="{name: 'Glossary'}">Glossary</b-dropdown-item>
+              <b-dropdown-item :to="{name: 'FAQ'}">FAQ</b-dropdown-item>
+              <b-dropdown-item v-on:click="mobile">Mobile Site</b-dropdown-item>
+            </b-nav-item-dropdown>
 
-      <b-nav-form>
-        <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search" />
-        <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-      </b-nav-form>
+            <b-nav-item-dropdown text="Other Stuff" right>
+              <b-dropdown-item :to="{name: 'Sitemap'}">Sitemap</b-dropdown-item>
+              <b-dropdown-item :to="{name: 'MajorDistros'}">Major Distributions</b-dropdown-item>
+              <b-dropdown-item :to="{name: 'SubmitDistro'}">Submit Distribution</b-dropdown-item>
+              <b-dropdown-item :to="{name: 'Home'}">Upcoming Releases</b-dropdown-item>
+            </b-nav-item-dropdown>
 
-      <b-nav is-nav-bar>
-        <b-nav-item :to="{name: 'Distro'}">FreeBSD</b-nav-item>
-      </b-nav>
-
-      <!-- Right aligned nav items -->
-      <b-nav is-nav-bar class="ml-auto">
-
-        <b-nav-item-dropdown text="Main topics" right>
-          <b-dropdown-item :to="{name: 'Headlines'}">Headlines</b-dropdown-item>
-          <b-dropdown-item :to="{name: 'Weekly'}">DW Weekly</b-dropdown-item>
-          <b-dropdown-item disabled>Comments</b-dropdown-item>
-          <hr>
-          <b-dropdown-item :to="{name: 'Packages'}">Packages</b-dropdown-item>
-          <b-dropdown-item :to="{name: 'PackageManagement'}">Package Management</b-dropdown-item>
-          <hr>
-          <b-dropdown-item :to="{name: 'Glossary'}">Glossary</b-dropdown-item>
-          <b-dropdown-item :to="{name: 'FAQ'}">FAQ</b-dropdown-item>
-          <b-dropdown-item v-on:click="mobile">Mobile Site</b-dropdown-item>
-        </b-nav-item-dropdown>
-
-        <b-nav-item-dropdown text="Other Stuff" right>
-          <b-dropdown-item :to="{name: 'Sitemap'}">Sitemap</b-dropdown-item>
-          <b-dropdown-item :to="{name: 'MajorDistros'}">Major Distributions</b-dropdown-item>
-          <b-dropdown-item :to="{name: 'SubmitDistro'}">Submit Distribution</b-dropdown-item>
-          <b-dropdown-item :to="{name: 'Home'}">Upcoming Releases</b-dropdown-item>
-        </b-nav-item-dropdown>
-
-        <b-nav-item-dropdown text="About" right>
-          <b-dropdown-item :to="{name: 'Home'}">About DistroWatch</b-dropdown-item>
-          <b-dropdown-item :to="{name: 'Home'}">Page Hit Ranking</b-dropdown-item>
-          <b-dropdown-item :to="{name: 'Home'}">Advertise</b-dropdown-item>
-          <b-dropdown-item :to="{name: 'Home'}">CryptoCoin.cc</b-dropdown-item>
-        </b-nav-item-dropdown>
-
-      </b-nav>
-
-    </b-collapse>
-  </b-navbar>
-
+            <b-nav-item-dropdown text="About" right>
+              <b-dropdown-item :to="{name: 'Home'}">About DistroWatch</b-dropdown-item>
+              <b-dropdown-item :to="{name: 'Home'}">Page Hit Ranking</b-dropdown-item>
+              <b-dropdown-item :to="{name: 'Home'}">Advertise</b-dropdown-item>
+              <b-dropdown-item :to="{name: 'Home'}">CryptoCoin.cc</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+    <div class="underlay"></div>
   </div>
 </template>
 
@@ -70,6 +66,14 @@
 <style>
   .navbar {
     background-color: #F6EDC8;
+  }
+  .header {
+    position: fixed;
+    width: 100%;
+    z-index: 1;
+  }
+  .underlay {
+    height: 50px;
   }
 </style>
 
